b4d1d1910507921b1ec9ae94e34aefad24760d63
Sam Sprouse <samsprouse23@gmail.com>
2022-04-25 15:56:34 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,8 +1,7 @@
 module "s3-bucket" {
   source  = "app.terraform.io/sam-sprouse-hashicatlab/s3-bucket/aws"
   version = "2.8.0"
-  
-  bucket = "guarev-s3-bucket"
+
   bucket_prefix = "ssprouse"
   acl    = "private"
 
